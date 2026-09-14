90becfeafa8f1a1f37b1ac4a41f548f8cefd1516
windapermatasari-moon <windapermatasari1807@gmail.com>
2026-09-15 00:26:32 +0700

    Delete src directory

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://aucdcaethhzxmuhpcrpd.supabase.co'
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_Fdkca2TLfOEwoeQ4Un95Eg_BWW5jTaY'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY)
